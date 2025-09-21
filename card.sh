4ada3cecbef1b11411987cc0ddfc55e0c2f57c7b
pramodphuyal-grepsr <pramod.phuyal@grepsr.com>
2025-09-21 15:23:45 +0545

    test paths 4

--- card.sh
+++ card.sh
@@ -6,13 +6,19 @@ SCRIPT_DIR="/var/task/public/curl"
 
 source "$SCRIPT_DIR/config.sh"
 
-
 source "$SCRIPT_DIR/box.sh" 
 
-source "$SCRIPT_DIR/about.sh"
+# source "$SCRIPT_DIR/about.sh"
+
+source "$SCRIPT_DIR/horizontalLayout.sh"
+
+createHorizontalLayout  "$SCRIPT_DIR/name.sh" "$SCRIPT_DIR/info.sh"
+
 
 echo -e "\n"
-source "$SCRIPT_DIR/custom-details.sh"
+# source "$SCRIPT_DIR/custom-details.sh"
+
+createHorizontalLayout  "$SCRIPT_DIR/socials.sh" "$SCRIPT_DIR/neofetch.sh"
 
 
 source "$SCRIPT_DIR/footer.sh"
