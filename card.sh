f0e243af6d4603cc380650c27dd62b40571ceb24
pramodphuyal-grepsr <pramod.phuyal@grepsr.com>
2025-09-21 15:31:44 +0545

    test paths 5

--- card.sh
+++ card.sh
@@ -10,8 +10,6 @@ source "$SCRIPT_DIR/box.sh"
 
 # source "$SCRIPT_DIR/about.sh"
 
-source "$SCRIPT_DIR/horizontalLayout.sh"
-
 createHorizontalLayout  "$SCRIPT_DIR/name.sh" "$SCRIPT_DIR/info.sh"
 
 
@@ -23,3 +21,45 @@ createHorizontalLayout  "$SCRIPT_DIR/socials.sh" "$SCRIPT_DIR/neofetch.sh"
 
 source "$SCRIPT_DIR/footer.sh"
 
+
+
+
+
+
+
+
+
+createHorizontalLayout () {
+  local spacing="             "  # space between columns
+
+  # read outputs
+  # mapfile -t left  < <(bash "$1")
+  # mapfile -t right < <(bash "$2")
+
+    bash "$1" | mapfile -t left
+    bash "$2" | mapfile -t right
+
+  # find max width of left column (ignoring colors)
+  local maxlen=0
+  for l in "${left[@]}"; do
+    local len
+    len=$(strip_colors "$l" | awk '{print length}')
+    (( len > maxlen )) && maxlen=$len
+  done
+
+  # find max line count
+  local lines=$(( ${#left[@]} > ${#right[@]} ? ${#left[@]} : ${#right[@]} ))
+
+  # render
+  for ((i=0; i<lines; i++)); do
+    local l="${left[i]:-}"
+    local r="${right[i]:-}"
+    local clean_l=$(strip_colors "$l")
+    local pad=$(( maxlen - ${#clean_l} ))
+    printf "%s%s%*s%s%s%s\n" \
+      "$C_YELLOW" "$l" "$pad" "" \
+      "$spacing" \
+      "$C_DEFAULT" "$r"
+  done
+  printf "%s\n" "$C_DEFAULT"
+}
